a59f0895f8579f8acf71ed70bbcf7c022ed31ddc
SimonBurdy <s.burdy1789@gmail.com>
2022-10-10 09:20:16 +0200

    delete .idea

---
+++ lib/model/trivial_user.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'trivial_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_TrivialUser _$$_TrivialUserFromJson(Map<String, dynamic> json) =>
+    _$_TrivialUser(
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$_TrivialUserToJson(_$_TrivialUser instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
