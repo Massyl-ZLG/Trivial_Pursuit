75d0541426991b34df6d6b779f68d3a45ae674ae
SimonBurdy <simon.burdy@lacatholille.fr>
2022-12-03 11:18:43 +0100

    user repo / cubit / page in progress

--- lib/page/leaderboard/leaderboard_page.dart
+++ lib/page/leaderboard/leaderboard_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_flutter/data/dataSources/repositories/user_repository.dart';
+import 'package:test_flutter/model/trivial_user.dart';
+import 'package:test_flutter/page/leaderboard/bloc/leaderboard_cubit.dart';
+import 'package:test_flutter/page/leaderboard/bloc/leaderboard_state.dart';
 
 class LeaderboardPage extends StatefulWidget {
   const LeaderboardPage({Key? key}) : super(key: key);
@@ -8,20 +13,36 @@ class LeaderboardPage extends StatefulWidget {
 }
 
 class _LeaderboardPageState extends State<LeaderboardPage> {
-  List ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  LeaderboardCubit? cubit;
+
+  List<TrivialUser> _users = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children:
-          List.generate(ranks.length, (index) {
-            return ListTile(
-              title
-                  : Text(ranks[index].toString()),
-              leading
-                  : const Icon(Icons.people), );
-          }),
-        ),
+        body: RepositoryProvider(
+          create: (context) => UserRepository.getInstance(),
+          child: BlocProvider(
+            create : (context) {
+              cubit = LeaderboardCubit(
+                  repository: RepositoryProvider.of<UserRepository>(context));
+              return cubit!..fetchUsers();
+            },
+            child: BlocConsumer<LeaderboardCubit , LeaderboardState>(
+              listener: (context, state) {},
+              builder: (context, state) {
+                if(state is Loaded){
+                  return  Text('lool');
+                }
+                if(state is Error){
+
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+          ),
+        )
     );
   }
 }
