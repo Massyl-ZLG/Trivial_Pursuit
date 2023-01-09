8dc99172af3cb0b42b69b1656e637c3368e57ff4
SimonBurdy <s.burdy1789@gmail.com>
2023-01-09 23:05:48 +0100

    fix autogenerate mason f***ing file

--- lib/model/question_api_response.g.dart
+++ lib/model/question_api_response.g.dart
@@ -17,5 +17,7 @@ _$_QuestionApiResponse _$$_QuestionApiResponseFromJson(
 Map<String, dynamic> _$$_QuestionApiResponseToJson(
         _$_QuestionApiResponse instance) =>
     <String, dynamic>{
-      'results': instance.results,
+      'results': instance.results
+          .map((e) =>  e.toJson())
+          .toList(),
     };
